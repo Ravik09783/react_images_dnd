3d7ef173c2b13aa550db42c5e33b91337f5d05ff
Ravi kumar <ravik09783@gmail.com>
2024-10-26 07:49:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import { arrayMove, SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
+import { useState } from 'react';
+import { SortableItem } from './SortableItem';
+import AllImages from './assets/imags';
+
+function App() {
+  const [imageUrls, setImageUrls] = useState([AllImages.First, AllImages.Second, AllImages.Third,AllImages.Fourth, AllImages.Fifth]);
+
+  return (
+    <DndContext
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+    >
+      <Container className="p-3" style={{ width: "50%" }} align="center">
+        <h3>Sortable Images</h3>
+        <SortableContext
+          items={imageUrls}
+          strategy={verticalListSortingStrategy}
+        >
+          {imageUrls.map((imageUrl) => (
+            <SortableItem key={imageUrl} id={imageUrl} />
+          ))}
+        </SortableContext>
+      </Container>
+    </DndContext>
+  );
+
+  function handleDragEnd(event) {
+    const { active, over } = event;
+
+    if (over && active.id !== over.id) {
+      setImageUrls((items) => {
+        const activeIndex = items.indexOf(active.id);
+        const overIndex = items.indexOf(over.id);
+        return arrayMove(items, activeIndex, overIndex);
+      });
+    }
+  }
+}
+
+export default App;
---
+++ src/SortableItem.js
@@ -0,0 +1,26 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import Card from 'react-bootstrap/Card';
+
+export function SortableItem(props) {
+    const {
+        attributes,
+        listeners,
+        setNodeRef,
+        transform,
+        transition
+    } = useSortable({ id: props.id });
+
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition
+    };
+
+    return (
+        <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+            <Card body className="m-3">
+                <img src={props.id} alt="Sortable item" style={{ width: '20%' }} />
+            </Card>
+        </div>
+    );
+}
